2892256c151137b489ccd455a22fe55047be2564
Vitorgpc <vitorcolazelli@gmail.com>
2020-08-08 02:33:44 -0300

    att web

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,43 @@
 import React from 'react';
+
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
 
 function TeacherForm(){
     return(
-        <div id="page-teacher-list" className="container">
-            <PageHeader title="Que incrivel que voce quer dar aulas."/>
+        <div id="page-teacher-form" className="container">
+            <PageHeader 
+                title="Que incrivel que voce quer dar aulas."
+                description="O Primeiro passo é preencher esse formulario de inscrição."
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    <Input name="name" label="Nome Completo:" />
+                    <Input name="avatar" label="Avatar:" />
+                    <Input name="whatsapp" label="WhatsApp:" />
+                </fieldset>
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+                    <Input name="subject" label="Materia:" />
+                    <Input name="cost" label="Custo da sua hora por aula:" />
+                </fieldset>
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante"/>
+                        Importante! <br/>
+                        Preencha todos os dados
+                    </p>
+                    <button type="button">
+                        Salvar Cadastro
+                    </button>
+                </footer>
+            </main>
         </div>
     );
 }
